91245b3d63f47186ba5d0f5266d9d95e7453042d
th3controller <th3controller2000@gmail.com>
2012-12-13 20:22:31 -0500

    Added kick command, should issue reasons also

--- src/be/th3controller/simplerthanessentials/SimplerThanEssentials.java
+++ src/be/th3controller/simplerthanessentials/SimplerThanEssentials.java
@@ -122,6 +122,69 @@ public class SimplerThanEssentials extends JavaPlugin{
 				return true;
 			}
 		}
+		else if(cmd.getName().equalsIgnoreCase("kick")){
+			if(!(sender instanceof Player)){
+				if(args.length >= 2){
+					Player other = Bukkit.getServer().getPlayer(args[0]);
+					if(other == null){
+						sender.sendMessage(ChatColor.RED + args[0] + " is not online!");
+					}else{
+						StringBuilder b = new StringBuilder();
+						/* 57 */        for (int i = 1; i < args.length; i++) {
+						/* 58 */          if (i != 1)
+						/* 59 */            b.append(" ");
+						/* 60 */          b.append(args[i]);
+						}
+						other.kickPlayer("You have been kicked. Reason: " + b);
+					}
+				}
+				else if(args.length == 1){
+					Player other = Bukkit.getServer().getPlayer(args[0]);
+					if(other == null){
+						sender.sendMessage(ChatColor.RED + args[0] + " is not online!");
+					}else{
+						other.kickPlayer("You have been kicked. Reason: Undefined");
+					}
+				}
+				else if(args.length < 1){
+					sender.sendMessage(ChatColor.RED + "Incorrect arguments!");
+					sender.sendMessage(ChatColor.RED + "Usage: /kick <Player> [reason]");
+				}
+				return true;
+			}else{
+				if(!((Player)sender).hasPermission("ste.kick")){
+					((Player)sender).sendMessage(ChatColor.RED + "Insufficient permissions to do this command!");
+				}else{
+					if(args.length >= 2){
+						Player other = Bukkit.getServer().getPlayer(args[0]);
+						if(other == null){
+							sender.sendMessage(ChatColor.RED + args[0] + " is not online!");
+						}else{
+							StringBuilder b = new StringBuilder();
+							/* 57 */        for (int i = 1; i < args.length; i++) {
+							/* 58 */          if (i != 1)
+							/* 59 */            b.append(" ");
+							/* 60 */          b.append(args[i]);
+							}
+							other.kickPlayer("You have been kicked. Reason: " + b);
+						}
+					}
+					else if(args.length == 1){
+						Player other = Bukkit.getServer().getPlayer(args[0]);
+						if(other == null){
+							sender.sendMessage(ChatColor.RED + args[0] + " is not online!");
+						}else{
+							other.kickPlayer("You have been kicked. Reason: Undefined");
+						}
+					}
+					else if(args.length < 1){
+						sender.sendMessage(ChatColor.RED + "Incorrect arguments!");
+						sender.sendMessage(ChatColor.RED + "Usage: /kick <Player> [reason]");
+					}
+				}
+				return true;
+			}
+		}
 		return false;
 	}
 }
